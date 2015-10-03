1d8773715d0a19db88800b7ce6e653d062ffc794
hshockney <shockney@atlanticbb.net>
2015-10-03 12:54:32 -0400

    three parameters name, multiply

--- myapp.rb
+++ myapp.rb
@@ -10,5 +10,9 @@ get '/hello/:name' do
 	end
 
 get '/add/:value1/:value2' do |val1, val2|
-"These values =#{val1.to_i + val2.to_i}"
+	"These values =#{val1.to_i + val2.to_i}"
+end
+
+get '/multiply/:name/:int1/:int2' do |name,val1, val2|
+"Hello #{params['name']}, These values = #{val1.to_i * val2.to_i}"
 end
